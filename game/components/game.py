1ec2533b1cad0311de563418523fedb26460dba3
JesusRodriguez0307 <David.Rodriguez@fundacion-jala.org>
2023-06-13 18:58:42 -0500

    finalizando clase 2
    Agregamos enemigos sin interaccion, solo movimiento

--- game/components/game.py
+++ game/components/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
 from game.components.spaceship import Spaceship
+from game.components.enemies.enemy_manager import EnemyManager
 
 class Game:
     def __init__(self):
@@ -17,6 +18,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 0
         self.player = Spaceship()
+        self.enemy_manager = EnemyManager()
         
     def run(self):
         self.playing = True
@@ -36,6 +38,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.enemy_manager.update()
     
     def draw(self):
         self.clock.tick(FPS)
@@ -43,7 +46,7 @@ class Game:
         
         self.draw_background()
         self.player.draw(self.screen)
-            
+        self.enemy_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
         
