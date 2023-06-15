7b785ab7edc867ba7539138b635f6cb252590462
JesusRodriguez0307 <David.Rodriguez@fundacion-jala.org>
2023-06-14 21:16:04 -0500

    despues de clases
    implementamos los disparos en enemigos y modifiqué la aparicion de nuevos enemigos para poder ir acorde a las clases

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,23 +1,28 @@
 from game.components.enemies.enemy import Enemy
-from game.components.enemies.enemy_2 import Enemy2
+import random
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
         
-    def update(self):
+    def update(self, game):
         self.add_enemy()
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
+            enemy.shoot(game.bullet_manager)
         
     def add_enemy(self):
-        if len(self.enemies) < 5:
+        enemy_type = random.randint(0, 1)
+        if enemy_type == 1:
             enemy = Enemy()
+        else:
+            x_speed = 5
+            y_speed = 2
+            move_x_for = [50, 120]
+            enemy = Enemy(enemy_type, x_speed, y_speed, move_x_for)
+            
+        if len(self.enemies) < 5:
             self.enemies.append(enemy)
-        
-        if len(self.enemies) == 5:
-            enemy2 = Enemy2()            
-            self.enemies.append(enemy2)
             
     def draw(self, screen):
         for enemy in self.enemies:
